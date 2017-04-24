7c4221cca6a5f30155553c735343757866657413
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-24 21:00:42 +0200

    plugin: Add base implementation and french translation

--- fr/functions.sh
+++ fr/functions.sh
@@ -1,17 +1,13 @@
-#!/bin/bash
-# Here you can define translations to be used in the plugin functions file
-# the below code is an sample to be reused:
-# 1) uncomment to function below
-# 2) replace XXX by your plugin name (short)
-# 3) remove and add your own translations
-# 4) you can the arguments $2, $3 passed to this function
-# 5) in your plugin functions.sh file, use it like this:
-#      say "$(pv_myplugin_lang the_answer_is "oui")"
-#      => Jarvis: La réponse est oui
+#!/usr/bin/env bash
 
-#pv_XXX_lang () {
-#    case "$1" in
-#        i_check) echo "Je regarde...";;
-#        the_answer_is) echo "La réponse est $2";;
-#    esac
-#} 
+jv_pg_dr_lang()
+{
+  case "$1" in
+    upload_file_success) echo "Upload du fichier dans dropbox effectué.";;
+    upload_file_failed) echo "Impossible d'uploader le fichier dans dropbox...";;
+    download_file_success) echo "Téléchargement du fichier depuis dropbox effectué.";;
+    download_file_failed) echo "Impossible de télécharger le fichier depuis dropbox...";;
+
+    *) jv_error "Erreur: Clef de traduction '$1' non reconnue. Merci de contacter le concepteur de ce plugin.";;
+  esac
+}
--- functions.sh
+++ functions.sh
@@ -1,8 +1,53 @@
-#!/bin/bash
-# Here you can create functions which will be available from the commands file
-# You can also use here user variables defined in your config file
-# To avoid conflicts, name your function like this
-# pg_XX_myfunction () { }
-# pg for PluGin
-# XX is a short code for your plugin, ex: ww for Weather Wunderground
-# You can use translations provided in the language folders functions.sh
+#!/usr/bin/env bash
+
+# Upload file to dropbox
+# $1 (string): File path to get the file to upload (example: "/tmp/README.md")
+# $2 (string): File path in dropbox to put the file (example: "README.md")
+# $3 (bool, optional): Silent ("True" for no Jarvis response, "False" or no value for Jarvis response)
+#
+# return (int): 0 if success, 1 if failed
+jv_pg_dr_upload_file()
+{
+  # Send request to dropbox
+  result=`python3 plugins/jarvis-dropbox/script/dropbox_handler.py --token "$var_jv_pg_dr_oauth2_token" --in "$1" --out "$2" --upload`
+
+  # Show the result to user (if requested)
+  if [[ $? -eq 0 ]]; then
+    if [[ ! $3 =~ "True" ]]; then
+      say "$(jv_pg_dr_lang upload_file_success)"
+    fi
+    return 0
+  else
+    if [[ ! $3 =~ "True" ]]; then
+      say "$(jv_pg_dr_lang upload_file_failed)"
+    fi
+  fi
+
+return 1
+}
+
+# Download file from dropbox
+# $1 (string): File path in dropbox to get the file to download (example: "README.md")
+# $2 (string): File path to put the file (example: "/tmp/README.md")
+# $3 (bool, optional): Silent ("True" for no Jarvis response, "False" or no value for Jarvis response)
+#
+# return (int): 0 if success, 1 if failed
+jv_pg_dr_download_file()
+{
+  # Send request to dropbox
+  result=`python3 plugins/jarvis-dropbox/script/dropbox_handler.py --token "$var_jv_pg_dr_oauth2_token" --in "$1" --out "$2" --download`
+
+  # Show the result to user (if requested)
+  if [[ $? -eq 0 ]]; then
+    if [[ ! $3 =~ "True" ]]; then
+      say "$(jv_pg_dr_lang download_file_success)"
+    fi
+    return 0
+  else
+    if [[ ! $3 =~ "True" ]]; then
+      say "$(jv_pg_dr_lang downloadd_file_failed)"
+    fi
+  fi
+
+return 1
+}
