73ea8d1db8fd22c043cd51804c695d2c05f22e83
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-24 20:59:43 +0200

    install/uninstall: Add python install and dropbox python script

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
-#!/bin/bash
-# Use only if you need to perform changes on the user system such as installing software
+#!/usr/bin/env bash
+
+# Install Python (python 3.x)
+[[ -z $(which python) ]] && sudo apt-get install python
+[[ -z $(which python3) ]] && sudo apt-get install python3
+
+# Install dropbox python library
+sudo pip install dropbox
+
+# Copy latest version of "dropbox sender/receiver" python script
+wget -P script/ https://raw.githubusercontent.com/QuentinCG/Base-Scripts/master/OS_Independent/utils/dropbox_handler.py
