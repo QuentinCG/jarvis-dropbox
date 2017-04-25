572a2e4d8a2421e9bc6d20ae08dbadaa29afee33
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-25 08:58:16 +0200

    install: Fix issue while installing dropbox python api

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-# Install Python (python 3.x)
+# Copy latest version of "dropbox sender/receiver" python script
+wget -P script/ https://raw.githubusercontent.com/QuentinCG/Base-Scripts/master/OS_Independent/utils/dropbox_handler.py
+
+# Install Python (python 2.7 and 3.x)
 [[ -z $(which python) ]] && sudo apt-get install python
+[[ -z $(which python2.7) ]] && sudo apt-get install python2.7
 [[ -z $(which python3) ]] && sudo apt-get install python3
 
 # Install dropbox python library
-sudo pip install dropbox
-
-# Copy latest version of "dropbox sender/receiver" python script
-wget -P script/ https://raw.githubusercontent.com/QuentinCG/Base-Scripts/master/OS_Independent/utils/dropbox_handler.py
+#sudo pip install dropbox
+git clone git://github.com/dropbox/dropbox-sdk-python.git
+cd dropbox-sdk-python
+sudo python2.7 setup.py install
+sudo python3 setup.py install
+cd ..
+rm -rf dropbox-sdk-python
